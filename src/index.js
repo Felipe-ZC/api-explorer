afd64d2d3490faa07a6adc4c6d7abae3909e3f93
Felipe-ZC <fzunigacar@gmail.com>
2020-01-20 17:51:18 -0800

    Added some useful comments

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ import InvalidConfig from './components/InvalidConfig';
 import config from './config/config.json';
 
 // TODO: Validate Config! If config is invalid, render an invalid
-// config component!
+// config component! We can use fs.readFile() instead, makes it 
+// possible to handle errors before file is brought into memory.
+// Import can only be ran at top level, so try-catching it is out
+// of the question...
+
+// NOTE: This app doesn't need a router but would be nice 
+// in case we wanted too add further functionality to it. 
 if(typeof(config) == 'undefined'  || !(config.url && config.method))
 	ReactDOM.render(<InvalidConfig/>, document.getElementById('root'));
 else 
